71b1aecf3ffb0677eedff0905a9916cffff10778
Engelbert <engelfreites97@gmail.com>
2025-02-25 22:30:47 -0500

    feat(users): create POST for create a new user

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter, HTTPException
-from ..models.user import UserBase, User
+from ..models.user import UserBase, User, CreateUser
 from db import SessionDep
 from sqlmodel import select
+import bcrypt
 
 router = APIRouter(tags=["User"])
+
+def hash_password(password : str) -> str:
+  salt = bcrypt.gensalt()
+  hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+  return hashed_password
 
 @router.get("/api/users", response_model=list[User], status_code=200)
 async def get_all_users(session: SessionDep):
@@ -21,3 +27,17 @@ async def get_one_user(user_id: int, session: SessionDep):
     raise HTTPException(status_code=404, detail="User not found")
   
   return user
+
+@router.post('/api/users', response_model= UserBase)
+async def create_user(user_data: CreateUser, session: SessionDep ):
+  user_db = user_data.model_dump()
+  password = user_data.password
+  hashed_password = hash_password(password)
+
+  user = User(**user_db, hashed_password= hashed_password )
+
+  session.add(user)
+  session.commit()
+  session.refresh(user)
+
+  return user 
